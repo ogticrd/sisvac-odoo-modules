6491163f48027fcebbb5e821c0ea090254c23dca
Jose Lopez <joselopezg8@gmail.com>
2021-02-20 20:50:13 -0400

    [REF] sisvac_appointments: use black code formatter

--- sisvac_appointments/__manifest__.py
+++ sisvac_appointments/__manifest__.py
@@ -1,22 +1,19 @@
 {
-    'name': "SISVAC Appointments",
-    'description': """
+    "name": "SISVAC Appointments",
+    "description": """
         SISVAC Appointments
     """,
-    'author': "OPTIC, Kevin Jiménez",
-    'website': "http://optic.gob.do",
-
+    "author": "OPTIC, Kevin Jiménez",
+    "website": "http://optic.gob.do",
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    'depends': ['calendar'],
-
-    'data': [
-        'security/ir.model.access.csv',
-        'data/data.xml',
-        'views/calendar_views.xml',
-        'views/product_template.xml',
-        'wizard/vaccination_wizard.xml',
-    ]
+    "category": "Uncategorized",
+    "version": "0.1",
+    "depends": ["calendar"],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/data.xml",
+        "views/calendar_views.xml",
+        "views/product_template.xml",
+        "wizard/vaccination_wizard.xml",
+    ],
 }
