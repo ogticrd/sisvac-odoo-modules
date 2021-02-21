35ae4ed70f006a305262fc410d25acfc9df5c8dd
Jose Lopez <joselopezg8@gmail.com>
2021-02-20 20:50:13 -0400

    [REF] sisvac_appointments: use black code formatter
    [REF] sisvac_appointments: remove all product related stuff
    
    [REF] sisvac_appointments: remove all product related stuff

--- sisvac_appointments/models/calendar_event.py
+++ sisvac_appointments/models/calendar_event.py
@@ -9,15 +9,17 @@ class CalendarEvent(models.Model):
     patient_id = fields.Many2one("res.partner")
     next_appointment_date = fields.Datetime(readonly=True)
     vaccination_appointment = fields.Boolean(readonly=True)
-    state = fields.Selection([
-        ("pending", "Pending"),
-        ("done", "Done"),
-        ("canceled", "Canceled")
-    ], default="pending", readonly=True)
+    state = fields.Selection(
+        [("pending", "Pending"), ("done", "Done"), ("canceled", "Canceled")],
+        default="pending",
+        readonly=True,
+    )
     dosis_number = fields.Integer(readonly=True)
     another_appointment_needed = fields.Boolean(readonly=True)
     lots = fields.Char(readonly=True)
-    vaccinator_id = fields.Many2one("res.partner", readonly=True, domain=[("is_vaccinator", "=", True)])
+    vaccinator_id = fields.Many2one(
+        "res.partner", readonly=True, domain=[("is_vaccinator", "=", True)]
+    )
     patient_firm = fields.Binary(readonly=True)
     got_symptoms = fields.Boolean("Got Symptoms?", readonly=True)
     symptoms = fields.Text(readonly=True)
@@ -29,32 +31,38 @@ class CalendarEvent(models.Model):
     @api.model
     def create(self, vals):
         if vals.get("appointment_number", "New") == "New":
-            vals["appointment_number"] = self.env['ir.sequence'].next_by_code('sisvac.appointment') or "/"
+            vals["appointment_number"] = (
+                self.env["ir.sequence"].next_by_code("sisvac.appointment") or "/"
+            )
             vals["name"] = vals["appointment_number"]
         return super(CalendarEvent, self).create(vals)
 
-    @api.constrains('patient_id', 'status', 'vaccination_appointment')
+    @api.constrains("patient_id", "status", "vaccination_appointment")
     def _check_sigle_active_appointment(self):
         for record in self:
             appointment_filter = [
                 ("vaccination_appointment", "=", True),
                 ("state", "=", "pending"),
-                ("patient_id", "=", record.patient_id.id)
+                ("patient_id", "=", record.patient_id.id),
             ]
 
             current_appointments = self.search_count(appointment_filter)
 
             if current_appointments > 1:
-                raise ValidationError(_("This patient have another active vaccination appointment. \
-                    \nEach patient only can have one active vaccination appointment."))
+                raise ValidationError(
+                    _(
+                        "This patient have another active vaccination appointment. \
+                    \nEach patient only can have one active vaccination appointment."
+                    )
+                )
 
     def action_put_vaccine(self):
 
         return {
-            'name': _('Put Vaccine'),
-            'type': 'ir.actions.act_window',
-            'view_type': 'form',
-            'view_mode': 'form',
-            'res_model': 'sisvac.vaccination.wizard',
-            'target': 'new'
+            "name": _("Put Vaccine"),
+            "type": "ir.actions.act_window",
+            "view_type": "form",
+            "view_mode": "form",
+            "res_model": "sisvac.vaccination.wizard",
+            "target": "new",
         }
--- sisvac_appointments/wizard/vaccination_wizard.py
+++ sisvac_appointments/wizard/vaccination_wizard.py
@@ -6,22 +6,24 @@ class Vaccination(models.TransientModel):
     _description = "Vaccination Wizard"
 
     lots = fields.Char()
-    vaccinator_id = fields.Many2one("res.partner", domain=[("is_vaccinator", "=", True)])
+    vaccinator_id = fields.Many2one(
+        "res.partner", domain=[("is_vaccinator", "=", True)]
+    )
     got_symptoms = fields.Boolean("Got Symptoms?")
     symptoms = fields.Text()
 
     def _get_vaccination_data(self):
         """Hook method to be extended on other modules"""
         return {
-            'lots': self.lots,
-            'vaccinator_id': self.vaccinator_id.id,
-            'got_symptoms': self.got_symptoms,
-            'symptoms': self.symptoms,
-            'state': 'done'
+            "lots": self.lots,
+            "vaccinator_id": self.vaccinator_id.id,
+            "got_symptoms": self.got_symptoms,
+            "symptoms": self.symptoms,
+            "state": "done",
         }
 
     def put_vaccine(self):
         self.ensure_one()
-        active_id = self._context.get('active_id')
-        appointment = self.env['calendar.event'].browse(active_id)
+        active_id = self._context.get("active_id")
+        appointment = self.env["calendar.event"].browse(active_id)
         appointment.write(self._get_vaccination_data())
