9145866922917bcdb8fd003df41161372cee97ef
Jose Lopez <joselopezg8@gmail.com>
2021-02-20 22:09:05 -0400

    [ADD] added main sisvac app

---
+++ sisvac_app/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "SISVAC",
+    "author": "OPTIC, José López",
+    "website": "http://optic.gob.do",
+    "category": "Uncategorized",
+    "version": "0.1",
+    "depends": ["sisvac_stock"],
+    "data": [
+        "data/sisvac_app_data.xml",
+    ],
+    'installable': True,
+    'application': True,
+}
