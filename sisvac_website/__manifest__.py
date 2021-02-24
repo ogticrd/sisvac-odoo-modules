110c89a69532e68746584831967fce0e68e723e9
Kevin Jimenez <kevinjimenezlorenzo@gmail.com>
2021-02-24 19:55:35 +0000

    Adding pre registration form

--- sisvac_website/__manifest__.py
+++ sisvac_website/__manifest__.py
@@ -4,7 +4,7 @@
     "website": "http://optic.gob.do",
     "category": "Uncategorized",
     "version": "0.1",
-    "depends": ["website", "sisvac_appointments"],
+    "depends": ["website"],
     "data": [
         "views/templates.xml",
     ],
--- sisvac_website/controllers/controllers.py
+++ sisvac_website/controllers/controllers.py
@@ -1,3 +1,7 @@
+import json
+import requests
+from werkzeug.utils import redirect
+
 from odoo import http
 from odoo.http import request
 
@@ -13,3 +17,42 @@ class Vaccination(http.Controller):
             'sisvac_website.sisvac_vaccination_website_form',
             {"country_states": country_states}
         )
+
+    @http.route('/sisvac/patient_api/<int:vat>', auth='none', cors="*")
+    def get_patient(self, vat):
+        patient_api = "https://citizens-api.digital.gob.do/api/citizens/basic-data?id="
+
+        try:
+            req = requests.get(patient_api + str(vat))
+            if req.status_code != requests.codes.ok:
+                return False
+        except requests.exceptions.ConnectionError as e:
+            return False
+        except requests.exceptions.Timeout as e:
+            return False
+
+        content = req.json()
+        patient_name = content.get("name").split(" ")[0]
+
+        return json.dumps({"patient_name": patient_name})
+
+    @http.route('/sisvac/update_patient/', type="http", auth='public', csrf=False, method='POST', website=True)
+    def update_patient_kk(self, **post):
+        if len(post) <= 0:
+            return
+
+        partner_data = {}
+        partner_allow_data = ["phone", "mobile", "state_id", "city", "email", "disability", "notes"]
+        partner = request.env["res.partner"].sudo().search([("vat", "=", post.get("vat"))], limit=1)
+        if not partner:
+            return
+
+        for f, v in post.items():
+            if v and v in partner_allow_data:
+                partner_data.update({f: v})
+
+        if partner_data:
+            partner_data.update({"country_id": 61})
+            partner.sudo().update(partner_data)
+
+        return redirect("/contactus-thank-you")
